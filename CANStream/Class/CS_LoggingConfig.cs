20bd00b799ada8ebd27007ac1db3a1f940d2ea16
Vincent <VincentBrault@Outlook.com>
2015-07-04 14:13:13 +0200

    Logging configuration dev start

---
+++ CANStream/Class/CS_LoggingConfig.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vincent
+ * Date: 04/07/2015
+ * Time: 14:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace CANStream
+{
+	/// <summary>
+	/// CANStream logging configuration classe
+	/// </summary>
+	public class CS_LoggingConfig
+	{
+		//Default constructor
+		public CS_LoggingConfig()
+		{
+		}
+	}
+}
